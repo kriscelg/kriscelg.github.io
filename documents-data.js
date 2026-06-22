dd9b19e85ce7c540d7921760066b4a9105052d07
kriscelg <91479853+kriscelg@users.noreply.github.com>
2026-06-22 15:10:00 -0500

    Update documents-data.js

--- documents-data.js
+++ documents-data.js
@@ -3,34 +3,127 @@
 // Policy Documents
 const POLICY_DOCUMENTS = [
     {
-        title: "Agreement Monitoring Policy",
-        fileName: "agreement-monitoring-policy.html",
-        uploadDate: "2026-01-07",
-        fileType: "HTML",
-        tags: ["policy", "monitoring", "agreements", "contracts"],
-        description: "Guidelines for monitoring transfer payment agreements and procurement agreements"
-    },
-    {
-        title: "Client Eligibility Guidelines",
-        fileName: "clientEligibilityGuidelines.html",
+        title: "Authorizing Quits to Enable Clients to Access EI Part Two Employment Benefits",
+        fileName: "documents/policy/authorizedQuits.html",
         uploadDate: "2025-12-24",
         fileType: "HTML",
-        tags: ["guidelines", "eligibility", "clients", "programs"],
-        description: "Guidelines to assist caseworkers in determining client eligibility for programs and services"
+        tags: ['Skills Development', "HTML"],
+        description: "This document outlines the policy and guidelines whereby caseworkers may authorize a client to quit their job to attend a sponsored program."
     },
     {
-        title: "Program Delivery Service Provider Guidelines",
-        fileName: "programDeliveryServiceProviderGuidelines.html",
-        uploadDate: "2025-12-04",
+        title: "Employment Assistance Services Program Parameters",
+        fileName: "documents/policy/easParameters.html",
+        uploadDate: "2025-12-22",
         fileType: "HTML",
-        tags: ["guidelines", "service providers", "contracts", "delivery"],
-        description: "Information required to assess, negotiate, document and deliver contracted service provider agreements"
-    }
+        tags: ["ETP", "Case Management", "HTML"],
+        description: ""
+    },
+
+    {
+        title: "Employment Partnerships Parameters",
+        fileName: "documents/policy/epParameters.html",
+        uploadDate: "2025-12-22",
+        fileType: "HTML",
+        tags: ["ETP", "Case Management", "HTML"],
+        description: ""
+    },
+
+    {
+        title: "Labour Market Partnerships Program Parameters",
+        fileName: "documents/policy/lmpParameters.html",
+        uploadDate: "2025-12-22",
+        fileType: "HTML",
+        tags: ["ETP", "Case Management", "HTML"],
+        description: ""
+    },
+
+    {
+        title: "Negotiated Financial Assistance Policy",
+        fileName: "documents/policy/lmpParameters.html",
+        uploadDate: "2025-12-22",
+        fileType: "HTML",
+        tags: ["Skills Development", "Case Management", "HTML"],
+        description: ""
+    },
+
+    {
+        title: "Notice of Personal Information and Personal Health Information Collection (FIPPA/PHIA)",
+        fileName: "documents/policy/noticeofPersonalInformationandPersonalHealthInformationCollection_FIPPA_PHIA.html",
+        uploadDate: "2025-12-23",
+        fileType: "HTML",
+        tags: ["Case Management", "HTML"],
+        description: ""
+    },
+
+    {
+        title: "Research and Innovation Program Parameters",
+        fileName: "documents/policy/riParameters.html",
+        uploadDate: "2025-12-22",
+        fileType: "HTML",
+        tags: ["ETP", "Case Management", "HTML"],
+        description: ""
+    },
+
+    {
+        title: "Self Employment Program Parameters",
+        fileName: "documents/policy/seParameters.html",
+        uploadDate: "2025-12-22",
+        fileType: "HTML",
+        tags: ["ETP", "Case Management", "HTML"],
+        description: ""
+    },
+
+    {
+        title: "Skills Development (SD) Program Parameters",
+        fileName: "documents/policy/skillsDevelopmentParameters.html",
+        uploadDate: "2025-12-22",
+        fileType: "HTML",
+        tags: ["Skills Development", "Case Management", "HTML"],
+        description: ""
+    },
+
+    {
+        title: "Wage Subsidy Program Parameters",
+        fileName: "documents/policy/wageSubsidyParameters.html",
+        uploadDate: "2025-12-22",
+        fileType: "HTML",
+        tags: ["Wage Subsidy", "Case Management", "HTML"],
+        description: ""
+    },
+
     // Add more policy documents here as they are created
+
 ];
 
-// Procedures & Guidelines
+// Job Aids & Guidelines
 const PROCEDURES_GUIDELINES = [
+
+    {
+        title: "Agreement Monitoring Policy",
+        fileName: "documents/jobAidsAndGuidelines/agreementMonitoringGuidelines.html",
+        uploadDate: "2025-12-3",
+        fileType: "HTML",
+        tags: ["ETP", "Indirect Services", "Service Provider Resource", "Case Management", "Overpayments"],
+        description: ""
+    },
+
+    {
+        title: "Client Eligibility Guidelines - Internal Use Only",
+        fileName: "documents/jobAidsAndGuidelines/clientEligibilityGuidelines.html",
+        uploadDate: "2025-12-24",
+        fileType: "HTML",
+        tags: ["Case Management", "Overpayments"],
+        description: ""
+    },
+
+    {
+        title: "Program Delivery Service Provider Guidelines",
+        fileName: "documents/jobAidsAndGuidelines/programDeliveryServiceProviderGuidelines.html",
+        uploadDate: "2025-12-4",
+        fileType: "HTML",
+        tags: ["ETP", "Service Provider Resource", "Case Management", "ICM", "Indirect Services"],
+        description: ""
+    }
     // Add procedure and guideline documents here
     // Example:
     // {
@@ -43,8 +136,9 @@ const PROCEDURES_GUIDELINES = [
     // }
 ];
 
-// Forms, Templates & Resources
-const FORMS_TEMPLATES_RESOURCES = [
+// Forms, Templates 
+const FORMS_TEMPLATES = [
+
     // Add forms, templates, and resource documents here
     // Example:
     // {
@@ -56,3 +150,180 @@ const FORMS_TEMPLATES_RESOURCES = [
     //     description: "Form for requesting project approval and funding"
     // }
 ];
+
+// Finance Operations
+const FINANCE_OPERATIONS = [
+
+    
+
+    // Add forms, templates, and resource documents here
+    // Example:
+    // {
+    //     title: "Project Approval Request (PAR) Form",
+    //     fileName: "forms/PAR-form.docx",
+    //     uploadDate: "2026-01-10",
+    //     fileType: "DOCX",
+    //     tags: ["form", "project approval", "funding"],
+    //     description: "Form for requesting project approval and funding"
+    // }
+];
+
+// Resources
+const RESOURCE_DOCS = [
+
+    {
+        title: "MJSD Slideshow English and French",
+        fileName: "documents/resources/mjsdCenterSlideshowEnglishAndFrench.pptx",
+        uploadDate: "2026-3-3",
+        fileType: "PowerPoint",
+        tags: ["Marketing", "General Service Delivery", "English", "French", "PowerPoint"],
+        description: ""
+    },
+
+    {
+        title: "Skills Development Fact Sheet English",
+        fileName: "documents/resources/skillsDevelopmentFactSheetEnglish.pdf",
+        uploadDate: "2026-3-3",
+        fileType: "PDF",
+        tags: ["Skills Development", "English", "PDF"],
+        description: ""
+    },
+
+    {
+        title: "Skills Development Fact Sheet French",
+        fileName: "documents/resources/skillsDevelopmentFactSheetFrench.pdf",
+        uploadDate: "2026-3-3",
+        fileType: "PDF",
+        tags: ["Skills Development", "French", "PDF"],
+        description: ""
+    },
+
+    {
+        title: "TES Presentation to General Public English",
+        fileName: "documents/resources/tesPresentationToGeneralPublicEnglish.pptx",
+        uploadDate: "2026-3-3",
+        fileType: "PowerPoint",
+        tags: ["Marketing", "General Service Delivery", "English", "PowerPoint"],
+        description: ""
+    },
+    {
+        title: "TES Presentation to General Public French",
+        fileName: "documents/resources/tesPresentationToGeneralPublicFrench.pptx",
+        uploadDate: "2026-3-3",
+        fileType: "PowerPoint",
+        tags: ["Marketing", "General Service Delivery", "French", "PowerPoint"],
+        description: ""
+    },
+    {
+        title: "TES Presentation to Stakeholders English",
+        fileName: "documents/resources/tesPresentationToStakeholdersEnglish.pptx",
+        uploadDate: "2026-3-3",
+        fileType: "PowerPoint",
+        tags: ["Marketing", "General Service Delivery", "English", "PowerPoint"],
+        description: ""
+    },
+
+    {
+        title: "TES Presentation to Stakeholders French",
+        fileName: "documents/resources/tesPresentationToStakeholdersFrench.pptx",
+        uploadDate: "2026-3-3",
+        fileType: "PowerPoint",
+        tags: ["Marketing", "General Service Delivery", "French", "PowerPoint"],
+        description: ""
+    },
+    {
+        title: "QR Code - Employer Supports English",
+        fileName: "documents/resources/qrCodeEmployerSupportsEnglish.png",
+        uploadDate: "2026-3-3",
+        fileType: "Image",
+        tags: ["Marketing", "General Service Delivery", "English", "Image"],
+        description: ""
+    },
+
+    {
+        title: "QR Code - Employer Supports French",
+        fileName: "documents/resources/qrCodeEmployerSupportsFrench.png",
+        uploadDate: "2026-3-3",
+        fileType: "Image",
+        tags: ["Marketing", "General Service Delivery", "French", "Image"],
+        description: ""
+    },
+
+    {
+        title: "QR Code - ETP Page English",
+        fileName: "documents/resources/qrCodeEtpPageEnglish.png",
+        uploadDate: "2026-3-3",
+        fileType: "Image",
+        tags: ["Marketing", "General Service Delivery", "English", "Image"],
+        description: ""
+    },
+
+    {
+        title: "QR Code - ETP Page French",
+        fileName: "documents/resources/qrCodeEtpPageFrench.png",
+        uploadDate: "2026-3-3",
+        fileType: "Image",
+        tags: ["Marketing", "General Service Delivery", "French", "Image"],
+        description: ""
+    },
+
+    {
+        title: "QR Code - MJSD Centres English",
+        fileName: "documents/resources/qrCodeMjsdCentersEnglish.png",
+        uploadDate: "2026-3-3",
+        fileType: "Image",
+        tags: ["Marketing", "General Service Delivery", "English", "Image"],
+        description: ""
+    },
+
+    {
+        title: "QR Code - MJSD Centres French",
+        fileName: "documents/resources/qrCodeMjsdCentersFrench.png",
+        uploadDate: "2026-3-3",
+        fileType: "Image",
+        tags: ["Marketing", "General Service Delivery", "French", "Image"],
+        description: ""
+    },
+
+    {
+        title: "QR Code - TES Index Page English",
+        fileName: "documents/resources/qrCodeTesIndexPageEnglish.png",
+        uploadDate: "2026-3-3",
+        fileType: "Image",
+        tags: ["Marketing", "General Service Delivery", "English", "Image"],
+        description: ""
+    },
+
+    {
+        title: "QR Code - TES Index Page French",
+        fileName: "documents/resources/qrCodeTesIndexPageFrench.png",
+        uploadDate: "2026-3-3",
+        fileType: "Image",
+        tags: ["Marketing", "General Service Delivery", "French", "Image"],
+        description: ""
+    },
+
+    {
+        title: "Alternate Funding Supports",
+        fileName: "documents/resources/alternateFundingSupports.html",
+        uploadDate: "2024-7-8",
+        fileType: "HTML",
+        tags: ["Case Management", "Skills Development", "EAPD", "Initiatives", "Client Resource", "English", "HTML"],
+        description: ""
+    }
+
+
+    // Add forms, templates, and resource documents here
+    // Example:
+    // {
+    //     title: "Project Approval Request (PAR) Form",
+    //     fileName: "forms/PAR-form.docx",
+    //     uploadDate: "2026-01-10",
+    //     fileType: "DOCX",
+    //     tags: ["form", "project approval", "funding"],
+    //     description: "Form for requesting project approval and funding"
+    // }
+];
+
+
+
